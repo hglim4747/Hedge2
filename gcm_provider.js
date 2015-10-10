2a15e051cf87662fc894fd31e54abb83de72c50a
Innocentias <ssje2006@naver.com>
2015-10-10 15:34:51 +0900

    yes

--- gcm_provider.js
+++ gcm_provider.js
@@ -19,7 +19,7 @@ var server_api_key = 'AIzaSyDwipCJy_lTDppme9GJNupB161sEe_x8CM';
 var sender = new gcm.Sender(server_api_key);
 var registrationIds = [];
 
-var token = 'd4AzfV_a49o:APA91bHgka-_xcCLTcq5zcvQ3RRZGo2yd1wAesVcalqFEIqz66AbcDUmNJiNjP29k53zqe7GUZy4BAbrZfRckpmehTliAmYeAjrToOKMwC9nOfIWdvdANFQnuvAjfV7lj3ooAEoAG0Yv';
+var token = 'cWA1YmNyPVk:APA91bFxhMb-KK3fCaOqUIlfHcOx79xPWi-_UjtCPmsyrVvG0qI0hC2sIFcfSuQce4Yug0ax9ihUniJ49jtGyaGCHrhw65Tj_UP3IeN9t0WABvZdZAKI54wibrjAbuboH1L3PFDE36Lx';
 registrationIds.push(token);
 
 sender.send(message, registrationIds, 4, function (err, result) {
